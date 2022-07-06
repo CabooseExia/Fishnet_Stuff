3114c20cc4078199dee673b87939d7ba13f78b88
Ethan <81744234+CabooseExia@users.noreply.github.com>
2022-07-06 10:31:21 +0800

    removed useless line

--- FishNet/rough_work.py
+++ FishNet/rough_work.py
@@ -8,14 +8,13 @@ path = 'C://Users//User//Desktop//food_dict.xlsx'
 
 def main(): # to help me copy pasta constant vars dict
     copy_pasta = {}
-    with open(path,'r') as yes:
-        temp = f'{path[:-4]}csv'
-        read_file = pd.read_excel(path)
-        read_file.to_csv(temp,index=None)
-        with open(temp, 'r') as rly:
-            maybe = csv.reader(rly)
-            for row in maybe:
-                copy_pasta[row[0]] = row[1]
+    temp = f'{path[:-4]}csv'
+    read_file = pd.read_excel(path)
+    read_file.to_csv(temp,index=None)
+    with open(temp, 'r') as rly:
+        maybe = csv.reader(rly)
+        for row in maybe:
+            copy_pasta[row[0]] = row[1]
     print(copy_pasta)
     pyperclip.copy(str(copy_pasta))
     os.remove(temp)
