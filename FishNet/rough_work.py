4e42c4a1cee8f645f286e845b30ea88eb81fd613
Ethan <81744234+CabooseExia@users.noreply.github.com>
2022-07-18 15:56:13 +0800

    Find diff barcode (and maybe internal code hehe) (#11)

--- FishNet/rough_work.py
+++ FishNet/rough_work.py
@@ -1,6 +1,7 @@
 import pdb
 import os
 import csv
+from numpy import double
 import pandas as pd
 import pyperclip
 
@@ -19,4 +20,12 @@ def main(): # to help me copy pasta constant vars dict
     pyperclip.copy(str(copy_pasta))
     os.remove(temp)
 
-main()
+# main()
+
+def double_loop():
+    for i in range(5):
+        for j in range(6):
+            if i == j:
+                print(f'{i},{j}')
+
+double_loop()
